86b541a7222e54fbbd1a123811acffbf5cb88f60
David <davidalmeida1620@gmail.com>
2023-06-21 19:24:46 -0500

    se creo el project-1

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+import { Header } from './components/Header';
+import { TaskList } from './components/TaskList';
+import { Flex,ChakraProvider} from '@chakra-ui/react';
+
+
+function App() {
+  
+  return (
+    
+    <ChakraProvider> 
+      <Flex
+       minH="100vh" 
+       display="flex"
+       alignItems="center"
+       justifyContent="center"
+       flexDirection="column"
+       bgGradient="linear(to bottom, rgba(34, 45, 195, 1), rgba(45, 189, 253, 1))" > 
+        <Header/>
+        <TaskList/>
+      </Flex>
+      </ChakraProvider>
+     
+    
+  )
+}
+
+export default App
