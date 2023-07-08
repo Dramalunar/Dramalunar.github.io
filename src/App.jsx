7a8281ce031dda6bf5f940a96e34374955ea3046
David <davidalmeida1620@gmail.com>
2023-07-07 21:19:23 -0500

    se añadio el project-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { Header } from './components/Header';
-import { TaskList } from './components/TaskList';
-import { Flex,ChakraProvider,extendTheme} from '@chakra-ui/react';
+import { Header } from "./components/Header";
+import { TaskList } from "./components/TaskList";
+import { Flex, ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { TaskProvider } from "./TaskContext";
 
 const theme = extendTheme({
   fonts: {
     body: "Caprasimo, sans-serif",
     heading: "Ubuntu, serif",
-    
   },
 });
 
 function App() {
-  
   return (
-    
-    <ChakraProvider theme={theme}> 
-      <Flex
-       minH="100vh" 
-       display="flex"
-       alignItems="center"
-       justifyContent="center"
-       flexDirection="column"
-       bgGradient="linear(to bottom, rgba(34, 45, 195, 1), rgba(45, 189, 253, 1))" > 
-        <Header />
-        <TaskList/>
-      </Flex>
-      </ChakraProvider>
-     
-    
-  )
+    <ChakraProvider theme={theme}>
+      <TaskProvider>
+        <Flex
+          minH="100vh"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="column"
+          bgGradient="linear(to bottom, rgba(34, 45, 195, 1), rgba(45, 189, 253, 1))"
+        >
+          <Header />
+          <TaskList />
+        </Flex>
+      </TaskProvider>
+    </ChakraProvider>
+  );
 }
 
-export default App
+export default App;
